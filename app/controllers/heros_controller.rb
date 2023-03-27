80208cf39aa9f03dc0a12462d3ed93c95eba94d4
Victor Aketch <vickeryace@gmail.com>
2023-03-27 07:38:42 +0300

    Generated app serializers

--- app/controllers/heros_controller.rb
+++ app/controllers/heros_controller.rb
@@ -1,2 +1,11 @@
 class HerosController < ApplicationController
+   def index
+    heroes = Hero.all
+    render json: heroes.as_json(only: [:id, :name, :super_name])
+   end
+
+   def show
+    hero = Hero.find(params[:id])
+    render json: hero 
+   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   resources :powers
-  resources :heros
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :heros, only: [:index, :show]
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
