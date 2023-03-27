b45bf54f1a1767503303eca8dbe234711e0841a2
Victor Aketch <vickeryace@gmail.com>
2023-03-27 07:01:50 +0300

    Created model relationships

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+# Create 10 random powers
+puts "📃 Seeding data..."
+
+10.times do
+  Power.create(
+    name: Faker::Superhero.power,
+    description: Faker::Lorem.sentence
+  )
+end
+
+# Create 10 random heroes, each with 1-3 random powers
+10.times do
+  hero = Hero.create(
+    name: Faker::Superhero.name,
+    super_name: Faker::Superhero.descriptor
+  )
+
+  # Add 1-3 random powers to the hero
+  rand(1..3).times do
+    power = Power.all.sample
+    HeroPower.create(hero: hero, power: power)
+  end
+end
+
+puts "✅ Done seeding"
